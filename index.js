33f9db81b8c91d139a4015509c08004bb12dad90
RohitDevendraSawant <rosawant51@gmail.com>
2024-02-26 10:53:33 +0530

    Basic client-server setup

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 
-const app = express();
 const PORT = 3000;
 
-app.listen(PORT, ()=>{
+const server = http.createServer();
+
+
+start = false
+
+
+
+const io = new Server(server);
+
+io.on('connection', (socket)=>{
+    console.log("Client connected.");
+
+    socket.on("message", (msg)=>{
+        console.log("Received message from clinet:", msg);
+        io.emit("message", "Message recieved");
+    })
+});
+
+server.listen(PORT, ()=>{
     console.log("Server running at http://localhost:"+PORT);
 })
